5a9ea5e1a0c3d64e3e276f922d9fc41fab79c02e
anshadat <88999918+anshadat@users.noreply.github.com>
2021-09-15 15:50:46 +0530

    rocky😇

--- plugins/owner.js
+++ plugins/owner.js
@@ -18,13 +18,31 @@ Asena.addCommand({pattern: 'owner', fromMe: false, desc: Lang.WP}, (async (messa
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Anshad*
-*owner number wa.me/919995390942*
-*instagram  : https://instagram.com/ans._had_?utm_medium=copy_link*
-*bot setting video : https://youtube.com/channel/UCQ3NYIDKT6-glPHTPya7b7w
-*githublink       https://github.com/anshadat/Rocky*
-*audio commads    https://github.com/anshadat/Rocky/tree/master/uploads*
-*sticker commads  https://github.com/anshadat/Rocky/tree/master/uploads*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭──────────────────╮
+   ᴀɴsʜᴀᴅ ᴠғᴄ
+╭──────────────────╯
+│
+│
+│
+│
+│ ▢ *ᴏᴡɴᴇʀ* : ᴀɴsʜᴀᴅ
+│ ▢ *ɴᴜᴍʙᴇʀ* : wa.me/919995390942
+│ ▢ *ɪɴsᴛᴀɢʀᴀᴍ* :https://instagram.com/ 
+│ans._had_?utm_medium=copy_link
+│ ▢ *ʏᴛ* :https://youtube.com/channel/│UCQ3NYIDKT6-glPHTPya7b7w
+│ ▢ *ɢɪᴛʜᴜʙ* :https://github.com/│anshadat/Rocky
+│ ▢ *ᴀᴜᴅɪᴏ ᴄᴏᴍɴᴅs* :https://github.com/│anshadat/Rocky/tree/master/uploads
+│ ▢ *sᴛɪᴄᴋᴇʀ ᴄᴏᴍɴᴅs* :https://│github.com/anshadat/Rocky/tree/│master/uploads
+│
+│      ▎▍▌▌▉▏▎▌▉▐▏▌▎
+│      ▎▍▌▌▉▏▎▌▉▐▏▌▎
+│       ©919995390942
+│
+╰──────────────────╮
+│ 
+│
+│   ❏ 𝘤𝘰𝘥𝘦𝘥 𝘣𝘺 *ᴀɴsʜᴀᴅ* ❏
+╰──────────────────╯
 `}) 
 
 }));
