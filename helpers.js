518c1a2102c29ff0a70ffe0592b029cc7ac66222
anshadat <88999918+anshadat@users.noreply.github.com>
2021-09-18 20:14:50 +0530

    Rocky✅

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *WhatsAsena*:  ```" + msg + "```"
+    return "config.SAID:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *WhatsAsena*:  ```" + msg + "```"
+    return "config.SAID:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *WhatsAsena*:  ```" + msg + "```"
+    return "config.SAID:  ```" + msg + "```"
 }
 
 
