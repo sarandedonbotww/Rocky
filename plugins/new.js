469c9f3b7c7956007fffaab3688b73fbc05b88ad
anshadat <88999918+anshadat@users.noreply.github.com>
2021-09-19 11:16:25 +0530

    Rocky✅

---
+++ plugins/new.js
@@ -0,0 +1,33 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'new', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://www.linkpicture.com/q/20210816_231658.jpg";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*New Bgm Added*
+
+Rockyser,Hy,Myre,Para,alive,da,Bot,Ara,Da,Hi,Bgm,Oo
+
+*New Features*
+
+*Create Your Own Bot*
+
+
+https://github.com/anshadat/Rocky
+`}) 
+
+}));
